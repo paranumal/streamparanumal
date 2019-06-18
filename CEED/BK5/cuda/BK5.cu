4fa0a88adcd5711f0a68d4efb2c2702646d7aa51
tcew <tim.warburton@gmail.com>
2019-06-18 11:58:46 -0400

    WIP: more migration

--- CEED/BK5/cuda/BK5.cu
+++ CEED/BK5/cuda/BK5.cu
@@ -8,10 +8,9 @@ See LICENSE file.
 #include <stdlib.h>
 #include <cuda.h>
 #include <cuda_runtime.h>
+#include "meshBasis.hpp"
 
-#define dfloat_t double
-
-void matrixPrint(int Nrows, int Ncols, dfloat_t *A, const char *mess){
+void matrixPrint(int Nrows, int Ncols, dfloat *A, const char *mess){
 #if 0
   printf("%s = [\n", mess);
   for(int i=0;i<Nrows;++i){
@@ -58,31 +57,20 @@ __forceinline__ __device__ __host__ int ijklN(const int i, const int j, const in
 #define NUM_DOFS_2D (NUM_DOFS_1D*NUM_DOFS_1D)
 #define NUM_DOFS_3D (NUM_DOFS_1D*NUM_DOFS_1D*NUM_DOFS_1D)
 
-#define p_Nggeo 7
+__constant__ dfloat const_DofToDofD[MAX_DOFS_1D*MAX_DOFS_1D];
+__constant__ dfloat const_oddDofToDofD[MAX_HALF_DOFS_1D*MAX_HALF_DOFS_1D];
+__constant__ dfloat const_evenDofToDofD[MAX_HALF_DOFS_1D*MAX_HALF_DOFS_1D];
 
-#define p_G00ID 0
-#define p_G01ID 1
-#define p_G02ID 2
-#define p_G11ID 3
-#define p_G12ID 4
-#define p_G22ID 5
-#define p_GWJID 6
+void randAlloc(int N, dfloat **h_a, dfloat **c_a){
 
-
-__constant__ dfloat_t const_DofToDofD[MAX_DOFS_1D*MAX_DOFS_1D];
-__constant__ dfloat_t const_oddDofToDofD[MAX_HALF_DOFS_1D*MAX_HALF_DOFS_1D];
-__constant__ dfloat_t const_evenDofToDofD[MAX_HALF_DOFS_1D*MAX_HALF_DOFS_1D];
-
-void randAlloc(int N, dfloat_t **h_a, dfloat_t **c_a){
-
-  *h_a = (dfloat_t*) calloc(N, sizeof(dfloat_t));
+  *h_a = (dfloat*) calloc(N, sizeof(dfloat));
 
   for(int n=0;n<N;++n)
     h_a[0][n] = drand48();
 
-  cudaMalloc(c_a, N*sizeof(dfloat_t));
+  cudaMalloc(c_a, N*sizeof(dfloat));
 
-  cudaMemcpy(c_a[0], h_a[0], N*sizeof(dfloat_t), cudaMemcpyHostToDevice);
+  cudaMemcpy(c_a[0], h_a[0], N*sizeof(dfloat), cudaMemcpyHostToDevice);
 
 }
 
@@ -93,17 +81,17 @@ template <int NUM_DOFS_1D, int p_Nblock >
   __forceinline__ __device__ 
   void BK5Device(const int numElements,
 		 const int element,
-		 const dfloat_t lambda,
-		 const dfloat_t * __restrict__ op,
-		 const dfloat_t * __restrict__ DofToDofD,
-		 const dfloat_t * __restrict__ oddDofToDofD,
-		 const dfloat_t * __restrict__ evenDofToDofD,
-		 dfloat_t * __restrict__ r_p,
-		 dfloat_t * __restrict__ r_Ap){
+		 const dfloat lambda,
+		 const dfloat * __restrict__ op,
+		 const dfloat * __restrict__ DofToDofD,
+		 const dfloat * __restrict__ oddDofToDofD,
+		 const dfloat * __restrict__ evenDofToDofD,
+		 dfloat * __restrict__ r_p,
+		 dfloat * __restrict__ r_Ap){
   
-  __shared__ dfloat_t s_p[p_Nblock][NUM_DOFS_1D][NUM_DOFS_1D];
-  __shared__ dfloat_t s_Gpr[p_Nblock][NUM_DOFS_1D][NUM_DOFS_1D];
-  __shared__ dfloat_t s_Gps[p_Nblock][NUM_DOFS_1D][NUM_DOFS_1D];
+  __shared__ dfloat s_p[p_Nblock][NUM_DOFS_1D][NUM_DOFS_1D];
+  __shared__ dfloat s_Gpr[p_Nblock][NUM_DOFS_1D][NUM_DOFS_1D];
+  __shared__ dfloat s_Gps[p_Nblock][NUM_DOFS_1D][NUM_DOFS_1D];
   
   // assumes NUM_DOFS_2D threads
   int t = threadIdx.x;
@@ -127,7 +115,7 @@ template <int NUM_DOFS_1D, int p_Nblock >
 
     __syncthreads();
     
-    dfloat_t G00 = 0, G01 =0, G02 =0, G11 =0, G12 =0, G22 =0, GWJ =0;
+    dfloat G00 = 0, G01 =0, G02 =0, G11 =0, G12 =0, G22 =0, GWJ =0;
     
     // prefetch geometric factors
     const int gbase = element*p_Nggeo*NUM_DOFS_3D + ijkN(i,j,k,NUM_DOFS_1D);
@@ -142,9 +130,9 @@ template <int NUM_DOFS_1D, int p_Nblock >
       GWJ = op[gbase+p_GWJID*NUM_DOFS_3D];
     }
     
-    dfloat_t pr = 0.f;
-    dfloat_t ps = 0.f;
-    dfloat_t pt = 0.f;
+    dfloat pr = 0.f;
+    dfloat ps = 0.f;
+    dfloat pt = 0.f;
 
 #pragma unroll
     for(int m = 0; m < NUM_DOFS_1D; m++) {
@@ -159,9 +147,9 @@ template <int NUM_DOFS_1D, int p_Nblock >
     s_Gpr[blk][j][i] = (G00*pr + G01*ps + G02*pt);
     s_Gps[blk][j][i] = (G01*pr + G11*ps + G12*pt);
     
-    dfloat_t Gpt = (G02*pr + G12*ps + G22*pt);
+    dfloat Gpt = (G02*pr + G12*ps + G22*pt);
     
-    dfloat_t Apk = GWJ*lambda*r_p[k];
+    dfloat Apk = GWJ*lambda*r_p[k];
     
     __syncthreads();
     
@@ -182,18 +170,18 @@ template <int NUM_DOFS_1D, int p_Nblock >
 
 template <int NUM_DOFS_1D, int p_Nblock >
 __global__ void BK5ConstantKernel(const int numElements,
-				  const dfloat_t lambda,
-				  const dfloat_t * __restrict__ op,
-				  const dfloat_t * __restrict__ DofToDofD,
-				  const dfloat_t * __restrict__ oddDofToDofD,
-				  const dfloat_t * __restrict__ evenDofToDofD,
-				  const dfloat_t * __restrict__ solIn,
-				  dfloat_t * __restrict__ solOut){
+				  const dfloat lambda,
+				  const dfloat * __restrict__ op,
+				  const dfloat * __restrict__ DofToDofD,
+				  const dfloat * __restrict__ oddDofToDofD,
+				  const dfloat * __restrict__ evenDofToDofD,
+				  const dfloat * __restrict__ solIn,
+				  dfloat * __restrict__ solOut){
   
-  __shared__ dfloat_t s_DofToDofD[NUM_DOFS_2D];
+  __shared__ dfloat s_DofToDofD[NUM_DOFS_2D];
 
-  dfloat_t r_q[NUM_DOFS_1D];
-  dfloat_t r_Aq[NUM_DOFS_1D];
+  dfloat r_q[NUM_DOFS_1D];
+  dfloat r_Aq[NUM_DOFS_1D];
 
   const unsigned int t = threadIdx.x;
   const int blk = threadIdx.y;
@@ -228,95 +216,14 @@ __global__ void BK5ConstantKernel(const int numElements,
   }
 }
 
-void BK5Host(int NUM_DOFS_1D, int numElements, dfloat_t lambda,
-	     const dfloat_t * __restrict__ op,
-	     const dfloat_t * __restrict__ DofToDofD,
-	     const dfloat_t * q,
-	     dfloat_t *lapqout){
-
-  
-  dfloat_t Gqr[NUM_DOFS_1D][NUM_DOFS_1D][NUM_DOFS_1D];
-  dfloat_t Gqs[NUM_DOFS_1D][NUM_DOFS_1D][NUM_DOFS_1D];
-  dfloat_t Gqt[NUM_DOFS_1D][NUM_DOFS_1D][NUM_DOFS_1D];
-  
-  for(int element=0;element<numElements;++element){
-    
-    for(int k=0;k<NUM_DOFS_1D;++k){
-      for(int j=0;j<NUM_DOFS_1D;++j){
-	for(int i=0;i<NUM_DOFS_1D;++i){
-	  
-	  dfloat_t qr = 0;
-	  dfloat_t qs = 0;
-	  dfloat_t qt = 0;
-	  
-	  for(int n=0;n<NUM_DOFS_1D;++n){
-	    int in = ijN(n,i,NUM_DOFS_1D);
-	    int jn = ijN(n,j,NUM_DOFS_1D);
-	    int kn = ijN(n,k,NUM_DOFS_1D);
-	    
-	    int kjn = ijklN(n,j,k,element,NUM_DOFS_1D);
-	    int kni = ijklN(i,n,k,element,NUM_DOFS_1D);
-	    int nji = ijklN(i,j,n,element,NUM_DOFS_1D);
-	    
-	    qr += DofToDofD[in]*q[kjn];
-	    qs += DofToDofD[jn]*q[kni];
-	    qt += DofToDofD[kn]*q[nji];	  
-	  }
-	  
-	  const int gbase = element*p_Nggeo*NUM_DOFS_3D + ijkN(i,j,k,NUM_DOFS_1D);
-	  
-	  dfloat_t G00 = op[gbase+p_G00ID*NUM_DOFS_3D];
-	  dfloat_t G01 = op[gbase+p_G01ID*NUM_DOFS_3D];
-	  dfloat_t G02 = op[gbase+p_G02ID*NUM_DOFS_3D];
-	  dfloat_t G11 = op[gbase+p_G11ID*NUM_DOFS_3D];
-	  dfloat_t G12 = op[gbase+p_G12ID*NUM_DOFS_3D];
-	  dfloat_t G22 = op[gbase+p_G22ID*NUM_DOFS_3D];
-	  
-	  Gqr[k][j][i] = (G00*qr + G01*qs + G02*qt);
-	  Gqs[k][j][i] = (G01*qr + G11*qs + G12*qt);
-	  Gqt[k][j][i] = (G02*qr + G12*qs + G22*qt);
-	}
-      }
-    }
-    
-    
-    for(int k=0;k<NUM_DOFS_1D;++k){
-      for(int j=0;j<NUM_DOFS_1D;++j){
-	for(int i=0;i<NUM_DOFS_1D;++i){
-	  
-	  int kji = ijklN(i,j,k,element,NUM_DOFS_1D);
-	  
-	  const int gbase = element*p_Nggeo*NUM_DOFS_3D + ijkN(i,j,k,NUM_DOFS_1D);
-	  
-	  dfloat_t GWJ = op[gbase+p_GWJID*NUM_DOFS_3D];
-	  dfloat_t lapq = lambda*GWJ*q[kji];
-	  
-	  for(int n=0;n<NUM_DOFS_1D;++n){
-	    int ni = ijN(i,n,NUM_DOFS_1D);
-	    int nj = ijN(j,n,NUM_DOFS_1D);
-	    int nk = ijN(k,n,NUM_DOFS_1D);
-	    
-	    lapq += DofToDofD[ni]*Gqr[k][j][n];
-	    lapq += DofToDofD[nj]*Gqs[k][n][i];
-	    lapq += DofToDofD[nk]*Gqt[n][j][i];	  
-	  }
-	  
-	  lapqout[kji] = lapq;
-	}
-      }
-    }
-  }
-}
-
-
 double bandwidthTest(cudaStream_t stream, int Ntests, size_t bwNtotal){
 
   cudaEvent_t start, end;
   cudaEventCreate(&start);
   cudaEventCreate(&end);	
   
-  dfloat_t *h_bwTest1, *c_bwTest1;
-  dfloat_t *h_bwTest2, *c_bwTest2;
+  dfloat *h_bwTest1, *c_bwTest1;
+  dfloat *h_bwTest2, *c_bwTest2;
   
   randAlloc(bwNtotal/2, &h_bwTest1, &c_bwTest1);
   randAlloc(bwNtotal/2, &h_bwTest2, &c_bwTest2);
@@ -325,8 +232,8 @@ double bandwidthTest(cudaStream_t stream, int Ntests, size_t bwNtotal){
   cudaEventRecord(start, stream);
   
   for(int test=0;test<Ntests/2;++test){
-    cudaMemcpy(c_bwTest2, c_bwTest1, (bwNtotal/2)*sizeof(dfloat_t), cudaMemcpyDeviceToDevice);
-    cudaMemcpy(c_bwTest1, c_bwTest2, (bwNtotal/2)*sizeof(dfloat_t), cudaMemcpyDeviceToDevice);
+    cudaMemcpy(c_bwTest2, c_bwTest1, (bwNtotal/2)*sizeof(dfloat), cudaMemcpyDeviceToDevice);
+    cudaMemcpy(c_bwTest1, c_bwTest2, (bwNtotal/2)*sizeof(dfloat), cudaMemcpyDeviceToDevice);
   }
   
   cudaEventRecord(end, stream);
@@ -338,7 +245,7 @@ double bandwidthTest(cudaStream_t stream, int Ntests, size_t bwNtotal){
   elapsed /= 1000.; // convert to s
   elapsed /= (double) Ntests;
   
-  double estimatedActualDeviceBandwidth = (bwNtotal*sizeof(dfloat_t)/elapsed)/1.e9;
+  double estimatedActualDeviceBandwidth = (bwNtotal*sizeof(dfloat)/elapsed)/1.e9;
   
   cudaFree(c_bwTest1);
   cudaFree(c_bwTest2);
@@ -354,16 +261,16 @@ double bandwidthTest(cudaStream_t stream, int Ntests, size_t bwNtotal){
 
 // leave this here in case we add odd-even versions
 void buildOddEvenMatrices(int NUM_COLS_OP, int NUM_ROWS_OP,
-			  dfloat_t *h_OP,   dfloat_t **c_OP, dfloat_t **c_oddOP,  dfloat_t **c_evenOP){
+			  dfloat *h_OP,   dfloat **c_OP, dfloat **c_oddOP,  dfloat **c_evenOP){
 
   int HALF_COLS_OP = ((NUM_COLS_OP+1)/2);
   int HALF_ROWS_OP = ((NUM_ROWS_OP+1)/2);
   
-  dfloat_t *X = (dfloat_t*) calloc(NUM_COLS_OP*NUM_COLS_OP, sizeof(dfloat_t));
-  dfloat_t *invX = (dfloat_t*) calloc(NUM_COLS_OP*NUM_COLS_OP, sizeof(dfloat_t));
+  dfloat *X = (dfloat*) calloc(NUM_COLS_OP*NUM_COLS_OP, sizeof(dfloat));
+  dfloat *invX = (dfloat*) calloc(NUM_COLS_OP*NUM_COLS_OP, sizeof(dfloat));
 
-  dfloat_t *cubX = (dfloat_t*) calloc(NUM_ROWS_OP*NUM_ROWS_OP, sizeof(dfloat_t));
-  dfloat_t *cubInvX = (dfloat_t*) calloc(NUM_ROWS_OP*NUM_ROWS_OP, sizeof(dfloat_t));
+  dfloat *cubX = (dfloat*) calloc(NUM_ROWS_OP*NUM_ROWS_OP, sizeof(dfloat));
+  dfloat *cubInvX = (dfloat*) calloc(NUM_ROWS_OP*NUM_ROWS_OP, sizeof(dfloat));
 
   for(int n=0;n<NUM_ROWS_OP;++n){
     cubX[n*NUM_ROWS_OP + n] = 1;
@@ -404,13 +311,13 @@ void buildOddEvenMatrices(int NUM_COLS_OP, int NUM_ROWS_OP,
   //  if(NUM_COLS_OP%2) invX[(NUM_COLS_OP)*(NUM_COLS_OP)/2] = 1;
   //  if(NUM_ROWS_OP%2) cubInvX[(NUM_ROWS_OP+1)*(NUM_ROWS_OP+1)/2] = 1;
   
-  dfloat_t *IinvX = (dfloat_t*) calloc(NUM_COLS_OP*NUM_ROWS_OP, sizeof(dfloat_t));
-  dfloat_t *cubInvXIinvX = (dfloat_t*) calloc(NUM_COLS_OP*NUM_ROWS_OP, sizeof(dfloat_t));
+  dfloat *IinvX = (dfloat*) calloc(NUM_COLS_OP*NUM_ROWS_OP, sizeof(dfloat));
+  dfloat *cubInvXIinvX = (dfloat*) calloc(NUM_COLS_OP*NUM_ROWS_OP, sizeof(dfloat));
 
   // post multiply by invX
   for(int i=0;i<NUM_ROWS_OP;++i){
     for(int a=0;a<NUM_COLS_OP;++a){
-      dfloat_t resI = 0;
+      dfloat resI = 0;
       for(int n=0;n<NUM_COLS_OP;++n){
 	resI += h_OP [i*NUM_COLS_OP+n]*invX[n*NUM_COLS_OP+a];
       }
@@ -421,7 +328,7 @@ void buildOddEvenMatrices(int NUM_COLS_OP, int NUM_ROWS_OP,
   // pre multiply by invX
   for(int i=0;i<NUM_ROWS_OP;++i){
     for(int a=0;a<NUM_COLS_OP;++a){
-      dfloat_t resI = 0;
+      dfloat resI = 0;
       for(int n=0;n<NUM_ROWS_OP;++n){
 	resI += cubInvX[i*NUM_ROWS_OP+n]*IinvX[n*NUM_COLS_OP + a];
       }
@@ -433,8 +340,8 @@ void buildOddEvenMatrices(int NUM_COLS_OP, int NUM_ROWS_OP,
   // [ A 0 ]  => [ A[0][0] B[0][0] A[0][1] B[0][1] .. A[0][HALF_DOFS_1D-1] B[0][HALF_DOFS_1D-1] .. 
   // [ 0 B ] 
 
-  dfloat_t *oddOP  = (dfloat_t*) calloc(NUM_ROWS_OP*HALF_ROWS_OP, sizeof(dfloat_t));
-  dfloat_t *evenOP = (dfloat_t*) calloc(NUM_ROWS_OP*HALF_ROWS_OP, sizeof(dfloat_t));
+  dfloat *oddOP  = (dfloat*) calloc(NUM_ROWS_OP*HALF_ROWS_OP, sizeof(dfloat));
+  dfloat *evenOP = (dfloat*) calloc(NUM_ROWS_OP*HALF_ROWS_OP, sizeof(dfloat));
   
   for(int i=0;i<HALF_ROWS_OP;++i){
     for(int a=0;a<HALF_COLS_OP;++a){
@@ -450,13 +357,13 @@ void buildOddEvenMatrices(int NUM_COLS_OP, int NUM_ROWS_OP,
   int NoddOP  = HALF_ROWS_OP*HALF_COLS_OP;
   int NevenOP = HALF_ROWS_OP*HALF_COLS_OP;
   
-  cudaMalloc(c_oddOP, NoddOP*sizeof(dfloat_t));
-  cudaMalloc(c_evenOP, NevenOP*sizeof(dfloat_t));
+  cudaMalloc(c_oddOP, NoddOP*sizeof(dfloat));
+  cudaMalloc(c_evenOP, NevenOP*sizeof(dfloat));
   
-  cudaMemcpy(*c_oddOP,  oddOP,  NoddOP*sizeof(dfloat_t),  cudaMemcpyHostToDevice);
-  cudaMemcpy(*c_evenOP, evenOP, NoddOP*sizeof(dfloat_t), cudaMemcpyHostToDevice);
+  cudaMemcpy(*c_oddOP,  oddOP,  NoddOP*sizeof(dfloat),  cudaMemcpyHostToDevice);
+  cudaMemcpy(*c_evenOP, evenOP, NoddOP*sizeof(dfloat), cudaMemcpyHostToDevice);
 
-  cudaMemcpy(*c_OP, h_OP,  NUM_COLS_OP*NUM_ROWS_OP*sizeof(dfloat_t),  cudaMemcpyHostToDevice);
+  cudaMemcpy(*c_OP, h_OP,  NUM_COLS_OP*NUM_ROWS_OP*sizeof(dfloat),  cudaMemcpyHostToDevice);
 
   matrixPrint(NUM_COLS_OP, NUM_COLS_OP, X, "X");
   matrixPrint(NUM_ROWS_OP, NUM_ROWS_OP, cubX, "cubX");
@@ -470,10 +377,10 @@ void buildOddEvenMatrices(int NUM_COLS_OP, int NUM_ROWS_OP,
 }
 
 
-void runBK5Kernel(cudaStream_t stream, int Nq, int numElements, dfloat_t lambda,
-		  dfloat_t *c_op,
-		  dfloat_t *c_DofToDofD, dfloat_t *c_oddDofToDofD, dfloat_t *c_evenDofToDofD,
-		  dfloat_t *c_solIn, dfloat_t *c_solOut){
+void runBK5Kernel(cudaStream_t stream, int Nq, int numElements, dfloat lambda,
+		  dfloat *c_op,
+		  dfloat *c_DofToDofD, dfloat *c_oddDofToDofD, dfloat *c_evenDofToDofD,
+		  dfloat *c_solIn, dfloat *c_solOut){
   
 #define BK5Kernel(Nq,Nblock)						\
   {									\
@@ -549,7 +456,7 @@ void runBK5Kernel(cudaStream_t stream, int Nq, int numElements, dfloat_t lambda,
 }
 
 
-dfloat_t nothingTest(cudaStream_t stream, int Ntests){
+dfloat nothingTest(cudaStream_t stream, int Ntests){
 
   cudaEvent_t start, end;
   cudaEventCreate(&start);
@@ -620,7 +527,7 @@ int main(int argc, char **argv){
   int          Nq = atoi(argv[1]);
   int numElements = atoi(argv[2]);
 
-  dfloat_t lambda = 0;
+  dfloat lambda = 0;
   
   printf("Running: NUM_DOFS_1D=%d, numElements=%d\n", Nq, numElements);
 
@@ -629,17 +536,16 @@ int main(int argc, char **argv){
 
   int    Ntotal = numElements*Np;
 
-  int Ntests = 100;
-
+  int Ntests = 10;
   
-  double estimatedActualDeviceBandwidth = bandwidthTest(stream, Ntests, (Ntotal*2+7*Ntotal)*sizeof(dfloat_t));
+  double estimatedActualDeviceBandwidth = bandwidthTest(stream, Ntests, (Ntotal*2+7*Ntotal)*sizeof(dfloat));
   
-  dfloat_t *h_op,      *c_op;
-  dfloat_t *h_solOut,       *c_solOut;
-  dfloat_t *h_solIn,        *c_solIn;
+  dfloat *h_op,      *c_op;
+  dfloat *h_solOut,       *c_solOut;
+  dfloat *h_solIn,        *c_solIn;
 
-  dfloat_t *h_DofToDofD,    *c_DofToDofD;
-  dfloat_t *c_oddDofToDofD, *c_evenDofToDofD;
+  dfloat *h_DofToDofD,    *c_DofToDofD;
+  dfloat *c_oddDofToDofD, *c_evenDofToDofD;
 
   // float fields
   randAlloc(Ntotal*p_Nggeo, &h_op, &c_op);
@@ -659,9 +565,9 @@ int main(int argc, char **argv){
   // create Odd-even packed storage for I and transpose(I) and push to constant memory
   buildOddEvenMatrices (Nq,Nq, h_DofToDofD, &c_DofToDofD, &c_oddDofToDofD, &c_evenDofToDofD);
 
-  cudaMemcpyToSymbol(const_DofToDofD,     c_DofToDofD,     Nq*Nq*sizeof(dfloat_t), 0, cudaMemcpyDeviceToDevice);
-  cudaMemcpyToSymbol(const_oddDofToDofD,  c_oddDofToDofD,  halfNq*halfNq*sizeof(dfloat_t), 0, cudaMemcpyDeviceToDevice);
-  cudaMemcpyToSymbol(const_evenDofToDofD, c_evenDofToDofD, halfNq*halfNq*sizeof(dfloat_t), 0, cudaMemcpyDeviceToDevice);
+  cudaMemcpyToSymbol(const_DofToDofD,     c_DofToDofD,     Nq*Nq*sizeof(dfloat), 0, cudaMemcpyDeviceToDevice);
+  cudaMemcpyToSymbol(const_oddDofToDofD,  c_oddDofToDofD,  halfNq*halfNq*sizeof(dfloat), 0, cudaMemcpyDeviceToDevice);
+  cudaMemcpyToSymbol(const_evenDofToDofD, c_evenDofToDofD, halfNq*halfNq*sizeof(dfloat), 0, cudaMemcpyDeviceToDevice);
   
   cudaEvent_t start, end;
   cudaEventCreate(&start);
@@ -669,7 +575,7 @@ int main(int argc, char **argv){
 
   // KERNEL GRID
   // do nothing kernel test
-  dfloat_t nothingElapsed = nothingTest(stream, Ntests);
+  dfloat nothingElapsed = nothingTest(stream, Ntests);
   nothingElapsed = nothingTest(stream, Ntests);
   
   // warm up call
@@ -725,7 +631,7 @@ int main(int argc, char **argv){
     elapsed /= 1000.;
     elapsed /= (double) Ntests;
 
-    int bytesMoved = (2*Np+7*Np)*sizeof(dfloat_t); // x, Mx, opa   
+    int bytesMoved = (2*Np+7*Np)*sizeof(dfloat); // x, Mx, opa   
     double bw = (bytesMoved*numElements/elapsed)/1.e9;
 
     double flopCount = Np*(6*2*Nq + 17);
@@ -738,18 +644,19 @@ int main(int argc, char **argv){
   }
 
   // check output is correct
-  BK5Host (Nq, numElements, lambda, h_op, h_DofToDofD, h_solIn, h_solOut);
+  //  BK5Host (Nq, numElements, lambda, h_op, h_DofToDofD, h_solIn, h_solOut);
+  meshReferenceBK5(Nq, numElements, lambda, h_op, h_DofToDofD, h_solIn, h_solOut);
 
-  // copy device version to host old q
-  dfloat_t *fromDevice = (dfloat_t*) calloc(numElements*Np, sizeof(dfloat_t));
-  cudaMemcpy(fromDevice, c_solOut, numElements*Np*sizeof(dfloat_t), cudaMemcpyDeviceToHost);
+  // copy device version to host old qh
+  dfloat *fromDevice = (dfloat*) calloc(numElements*Np, sizeof(dfloat));
+  cudaMemcpy(fromDevice, c_solOut, numElements*Np*sizeof(dfloat), cudaMemcpyDeviceToHost);
 
-  dfloat_t maxDiff = 0;
+  dfloat maxDiff = 0;
   
   for(int e=0;e<numElements;++e){
     for(int n=0;n<Np;++n){
       int id = e*Np + n;
-      dfloat_t diff = fabs(h_solOut[id]-fromDevice[id]);
+      dfloat diff = fabs(h_solOut[id]-fromDevice[id]);
       maxDiff = (diff>maxDiff) ? diff:maxDiff;
     }
   }
