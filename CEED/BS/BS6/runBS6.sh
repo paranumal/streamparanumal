d33bbafae8398b26b5e71c1a88616e655d47b5a3
Timothy Clive Warburton <tcew@corona98.llnl.gov>
2020-07-30 12:36:21 -0700

    WIP: BS6 run script

--- CEED/BS/BS6/runBS6.sh
+++ CEED/BS/BS6/runBS6.sh
@@ -45,14 +45,52 @@ make -j `nproc`
 
 echo "Running BS6..."
 
-mpirun -np 1 BS6 -m $mode -nx 159 -ny 159 -nz 159 -p 1
-mpirun -np 1 BS6 -m $mode -nx  80 -ny  80 -nz  80 -p 2
-mpirun -np 1 BS6 -m $mode -nx  53 -ny  53 -nz  53 -p 3
-mpirun -np 1 BS6 -m $mode -nx  40 -ny  40 -nz  40 -p 4
-mpirun -np 1 BS6 -m $mode -nx  32 -ny  32 -nz  32 -p 5
-mpirun -np 1 BS6 -m $mode -nx  27 -ny  27 -nz  27 -p 6
-mpirun -np 1 BS6 -m $mode -nx  23 -ny  23 -nz  23 -p 7
-mpirun -np 1 BS6 -m $mode -nx  20 -ny  20 -nz  20 -p 8
+for n in `seq 8 2 120`
+do
+    ./BS6 -m $mode -nx $n -ny $n -nz $n -p 1
+done
+
+for n in `seq 4 2 100`
+do
+    ./BS6 -m $mode -nx $n -ny $n -nz $n -p 2
+done
+
+for n in `seq 2 2 80`
+do
+    ./BS6 -m $mode -nx $n -ny $n -nz $n -p 3
+done
+
+for n in `seq 2 2 60`
+do
+    ./BS6 -m $mode -nx $n -ny $n -nz $n -p 4
+done
+
+
+for n in `seq 2 2 50`
+do
+    ./BS6 -m $mode -nx $n -ny $n -nz $n -p 5
+done
+
+
+for n in `seq 2 32`
+do
+    ./BS6 -m $mode -nx $n -ny $n -nz $n -p 6
+done
+
+
+for n in `seq 4 24`
+do
+    ./BS6 -m $mode -nx $n -ny $n -nz $n -p 7
+done
+
+
+#mpirun -np 1 BS6 -m $mode -nx  80 -ny  80 -nz  80 -p 2
+#mpirun -np 1 BS6 -m $mode -nx  53 -ny  53 -nz  53 -p 3
+#mpirun -np 1 BS6 -m $mode -nx  40 -ny  40 -nz  40 -p 4
+#mpirun -np 1 BS6 -m $mode -nx  32 -ny  32 -nz  32 -p 5
+#mpirun -np 1 BS6 -m $mode -nx  27 -ny  27 -nz  27 -p 6
+#mpirun -np 1 BS6 -m $mode -nx  23 -ny  23 -nz  23 -p 7
+#mpirun -np 1 BS6 -m $mode -nx  20 -ny  20 -nz  20 -p 8
 
 #
 # Noel Chalmers
