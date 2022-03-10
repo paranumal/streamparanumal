71b8fa8d28d7ae9ffb6b46dfb663096fef5d5fa9
Noel Chalmers <noel.chalmers@gmail.com>
2022-03-09 23:04:02 -0600

    Change default run sizes for BS6-8

--- BS/BS7/runBS7.sh
+++ BS/BS7/runBS7.sh
@@ -34,52 +34,59 @@ fi
 
 echo "Running BS7..."
 
-for n in `seq 8 2 120`
-do
-    ./BS7 -m $mode -nx $n -ny $n -nz $n -p 1
-done
+# for n in `seq 8 2 120`
+# do
+#     ./BS7 -m $mode -nx $n -ny $n -nz $n -p 1
+# done
 
-for n in `seq 4 2 100`
-do
-    ./BS7 -m $mode -nx $n -ny $n -nz $n -p 2
-done
+# for n in `seq 4 2 100`
+# do
+#     ./BS7 -m $mode -nx $n -ny $n -nz $n -p 2
+# done
 
-for n in `seq 2 2 80`
-do
-    ./BS7 -m $mode -nx $n -ny $n -nz $n -p 3
-done
+# for n in `seq 2 2 80`
+# do
+#     ./BS7 -m $mode -nx $n -ny $n -nz $n -p 3
+# done
 
-for n in `seq 2 2 60`
-do
-    ./BS7 -m $mode -nx $n -ny $n -nz $n -p 4
-done
-
-
-for n in `seq 2 2 50`
-do
-    ./BS7 -m $mode -nx $n -ny $n -nz $n -p 5
-done
+# for n in `seq 2 2 60`
+# do
+#     ./BS7 -m $mode -nx $n -ny $n -nz $n -p 4
+# done
 
 
-for n in `seq 2 32`
-do
-    ./BS7 -m $mode -nx $n -ny $n -nz $n -p 6
-done
+# for n in `seq 2 2 50`
+# do
+#     ./BS7 -m $mode -nx $n -ny $n -nz $n -p 5
+# done
 
 
-for n in `seq 4 24`
-do
-    ./BS7 -m $mode -nx $n -ny $n -nz $n -p 7
-done
+# for n in `seq 2 32`
+# do
+#     ./BS7 -m $mode -nx $n -ny $n -nz $n -p 6
+# done
 
 
-#mpirun -np 1 BS7 -m $mode -nx  80 -ny  80 -nz  80 -p 2
-#mpirun -np 1 BS7 -m $mode -nx  53 -ny  53 -nz  53 -p 3
-#mpirun -np 1 BS7 -m $mode -nx  40 -ny  40 -nz  40 -p 4
-#mpirun -np 1 BS7 -m $mode -nx  32 -ny  32 -nz  32 -p 5
-#mpirun -np 1 BS7 -m $mode -nx  27 -ny  27 -nz  27 -p 6
-#mpirun -np 1 BS7 -m $mode -nx  23 -ny  23 -nz  23 -p 7
-#mpirun -np 1 BS7 -m $mode -nx  20 -ny  20 -nz  20 -p 8
+# for n in `seq 4 24`
+# do
+#     ./BS7 -m $mode -nx $n -ny $n -nz $n -p 7
+# done
+
+mpirun -np 1 BS7 -m $mode -nx 126 -ny 126 -nz 126 -p 1
+mpirun -np 1 BS7 -m $mode -nx  80 -ny  80 -nz  80 -p 2
+mpirun -np 1 BS7 -m $mode -nx  53 -ny  53 -nz  53 -p 3
+mpirun -np 1 BS7 -m $mode -nx  40 -ny  40 -nz  40 -p 4
+mpirun -np 1 BS7 -m $mode -nx  32 -ny  32 -nz  32 -p 5
+mpirun -np 1 BS7 -m $mode -nx  27 -ny  27 -nz  27 -p 6
+mpirun -np 1 BS7 -m $mode -nx  23 -ny  23 -nz  23 -p 7
+mpirun -np 1 BS7 -m $mode -nx  20 -ny  20 -nz  20 -p 8
+mpirun -np 1 BS7 -m $mode -nx  18 -ny  18 -nz  18 -p 9
+mpirun -np 1 BS7 -m $mode -nx  16 -ny  16 -nz  16 -p 10
+mpirun -np 1 BS7 -m $mode -nx  15 -ny  15 -nz  15 -p 11
+mpirun -np 1 BS7 -m $mode -nx  14 -ny  14 -nz  14 -p 12
+mpirun -np 1 BS7 -m $mode -nx  13 -ny  13 -nz  13 -p 13
+mpirun -np 1 BS7 -m $mode -nx  12 -ny  12 -nz  12 -p 14
+mpirun -np 1 BS7 -m $mode -nx  11 -ny  11 -nz  11 -p 15
 
 #
 # Noel Chalmers
